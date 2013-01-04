16b6dc21317f75c2be7832ee6ce4485dedc3f2b6
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-01-04 09:31:46 -0800

    beginning to work on censusExcel

---
+++ censusExcel.php
@@ -0,0 +1,14 @@
+<?php
+
+/********************************************************
+*Ethan Eldridge 1/4/2013
+* 
+* This file creates an excel sheet that pulls data from the
+* api.census.gov website. This file requires the PHPExcel 
+* library to function. 
+*
+*/
+
+ require_once dirname(__FILE__) . '/../Classes/PHPExcel.php';
+
+?>
