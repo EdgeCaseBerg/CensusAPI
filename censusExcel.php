a9ab4c25c4cdad6abc064d0d3774551848f60202
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-01-04 10:40:41 -0800

    Added ignore for excel files and have headers being created

--- censusExcel.php
+++ censusExcel.php
@@ -9,6 +9,58 @@
 *
 */
 
- require_once dirname(__FILE__) . '/../Classes/PHPExcel.php';
+/** Error reporting */
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+require_once dirname(__FILE__) . '/../Classes/PHPExcel.php';
+
+//Create the excel object to use
+$excel = new PHPExcel();
+
+//Set Document Properties
+$excel->getProperties()->setCreator("Ethan Eldridge")
+					    ->setLastModifiedBy("Ethan Eldridge")
+					    ->setTitle("Census Information")
+						->setSubject("Census Information")
+						->setDescription("Spreadsheet using information from api.census.gov")
+						->setKeywords("Census,API,api.census.gov")
+						->setCategory("census");
+$excel->getActiveSheet()->setTitle("API Data Filled");
+
+
+$excel->setActiveSheetIndex(0);
+
+//Style the headers with color
+$excel->getActiveSheet()->getStyle('A1:E1')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
+$excel->getActiveSheet()->getStyle('A1:E1')->getFill()->getStartColor()->setARGB('FFFFFF99');
+$excel->getActiveSheet()->getStyle('B1')->getAlignment()->setTextRotation(90);
+
+//Set the widths of the header columns
+foreach (array('A','B','C','D','E') as $letter) {
+	$excel->getActiveSheet()->getColumnDimension($letter)->setAutoSize(true);	
+}
+
+//Set the height of the headers to be kinda big
+$excel->getActiveSheet()->getRowDimension(1)->setRowHeight(80);
+
+
+
+//Set the headers
+$excel->setActiveSheetIndex(0)
+      ->setCellValue('A1',"Text from Online Profile")
+      ->setCellValue('B1',"FieldName")
+      ->setCellValue('C1',"Table Number")
+      ->setCellValue('D1',"Computed Value")
+      ->setCellValue('E1',"Query to get Data");
+
+
+
+//save
+$objWriter = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
+$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+echo date('H:i:s') , " File written to " , str_replace('.php', '.xlsx', pathinfo(__FILE__, PATHINFO_BASENAME));
+
 
 ?>
